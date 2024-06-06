57201fffe7f91d24f9fc13af4d38681d8e667cb6
sanayamahajan-23 <monagupta9086287092@gmail.com>
2024-06-06 22:08:46 +0530

    add code to app.js

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const qrcode = require('qrcode');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set EJS as templating engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('index', { qrCodeUrl: null });
+});
+
+app.post('/generate', (req, res) => {
+    const url = req.body.url;
+
+    qrcode.toDataURL(url, (err, src) => {
+        if (err) res.send('Error occurred');
+
+        res.render('index', { qrCodeUrl: src });
+    });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
